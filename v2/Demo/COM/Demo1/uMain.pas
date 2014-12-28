579ba23fc13027abe6599433a94d9cee8d638a29
ismspider <ismspider@gmail.com>
2014-12-28 21:57:31 +0000

    Updated COM demo1.

--- v2/Demo/COM/Demo1/uMain.pas
+++ v2/Demo/COM/Demo1/uMain.pas
@@ -30,9 +30,15 @@ uses ShlObj, ComObj;
 {$R *.dfm}
 
 var
-  Trampoline_FileOpenDialog_Show: function(const Self, hwndParent: HWND): HRESULT; stdcall;
+  Trampoline_FileOpenDialog_Show: function(const Self; hwndParent: HWND): HRESULT; stdcall;
+  Trampoline_FileOpenDialog_SetTitle: function(const Self; pszTitle: LPCWSTR): HRESULT; stdcall;
 
-function FileOpenDialog_Show_Hook(const Self, hwndParent: HWND): HRESULT; stdcall;
+function FileOpenDialog_SetTitle_Hook(const Self; pszTitle: LPCWSTR): HRESULT; stdcall;
+begin
+  Result := Trampoline_FileOpenDialog_SetTitle(Self, 'Hooked');
+end;
+
+function FileOpenDialog_Show_Hook(const Self; hwndParent: HWND): HRESULT; stdcall;
 begin
   Main.MemLog.Lines.Add('Execution FileOpenDialog.Show ..');
   Result := Trampoline_FileOpenDialog_Show(Self, hwndParent);
@@ -52,6 +58,7 @@ procedure TMain.BtnEnableHookClick(Sender: TObject);
 begin
   if not Assigned(Trampoline_FileOpenDialog_Show) then
     @Trampoline_FileOpenDialog_Show := InterceptCreate(FileOpenDialog, 3, @FileOpenDialog_Show_Hook);
+  Trampoline_FileOpenDialog_SetTitle := InterceptCreate(FileOpenDialog, 17, @FileOpenDialog_SetTitle_Hook);
 end;
 
 procedure TMain.BtnDisableHookClick(Sender: TObject);
@@ -61,6 +68,7 @@ begin
     InterceptRemove(@Trampoline_FileOpenDialog_Show);
     Trampoline_FileOpenDialog_Show := nil;
   end;
+  InterceptRemove(@Trampoline_FileOpenDialog_SetTitle)
 end;
 
 initialization
