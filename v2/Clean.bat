7291c70e27a448e5cfe5cf4a4c8a56d089eb4ed2
ismspider <ismspider@gmail.com>
2015-01-01 22:53:39 +0000

    Updated Clean.bat

--- v2/Clean.bat
+++ v2/Clean.bat
@@ -1,7 +1,8 @@
 rem *****************************************
 rem *        Delphi CleanUp Batch.          *
 rem *                                       *
-rem * Clean identcache,local,dcu,exe files. *
+rem * Clean identcache,local,dcu,exe,       *
+rem * map,drc files.                        *
 rem * Clean hidden __history folder.        *
 rem *                                       *
 rem *        Author: Mahdi Safsafi          *
@@ -14,9 +15,20 @@ Del "*.identcache" /s/q
 Del "*.local" /s/q
 Del "*.dcu" /s/q
 Del "*.exe" /s/q
+Del "*.drc" /s/q
+Del "*.map" /s/q
 
-rem Delete hidden sub folder.
-For /f %%f in ('dir /s/b/l/a:dh') do (
-if %%~nf==__history rd /s/q %%f
+set mustdel=false
+For /f %%f in ('dir /s/b/l/a:d') do (
+    set "mustdel=false"
+	if %%~nf==Win32 (
+		if exist "%%f\Debug\" set "mustdel=true"
+		if exist "%%f\Release\" set "mustdel=true"
+) else if %%~nf==Win64 (
+		if exist "%%f\Debug\" set "mustdel=true"
+		if exist "%%f\Release\" set "mustdel=true" 
+		)
+if %%~nf==__history set "mustdel=true"
+if !mustdel!==true rd /s/q %%f
 )
-
+pause
