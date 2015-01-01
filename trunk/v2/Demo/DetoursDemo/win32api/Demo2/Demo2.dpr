a732349170d55b6119dea42a400bacf9bd5ed2e7
ismspider@gmail.com <ismspider@gmail.com@58b2790e-d512-84ac-7e0e-798749bd8ece>
2015-01-01 09:01:42 +0000

    Added InstDecodeDemo.

--- trunk/v2/Demo/DetoursDemo/win32api/Demo2/Demo2.dpr
+++ trunk/v2/Demo/DetoursDemo/win32api/Demo2/Demo2.dpr
@@ -3,9 +3,9 @@ program Demo2;
 uses
   Vcl.Forms,
   uMain in 'uMain.pas' {Main},
-  CPUID in '..\..\..\src\CPUID.pas',
-  DDetours in '..\..\..\src\DDetours.pas',
-  InstDecode in '..\..\..\src\InstDecode.pas';
+  CPUID in '..\..\..\..\src\CPUID.pas',
+  DDetours in '..\..\..\..\src\DDetours.pas',
+  InstDecode in '..\..\..\..\src\InstDecode.pas';
 
 {$R *.res}
 
