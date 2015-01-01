8b2ce2ac09a3ceeb5ac023ab6e3ee19b7b877439
ismspider <ismspider@gmail.com>
2015-01-01 19:15:27 +0000

    Removed __history folders.

--- v2/Clean.bat
+++ v2/Clean.bat
@@ -1,5 +1,22 @@
+rem *****************************************
+rem *        Delphi CleanUp Batch.          *
+rem *                                       *
+rem * Clean identcache,local,dcu,exe files. *
+rem * Clean hidden __history folder.        *
+rem *                                       *
+rem *        Author: Mahdi Safsafi          *
+rem *****************************************
+
 @echo off
+Setlocal EnableDelayedExpansion
+
 Del "*.identcache" /s/q
 Del "*.local" /s/q
 Del "*.dcu" /s/q
 Del "*.exe" /s/q
+
+rem Delete hidden sub folder.
+For /f %%f in ('dir /s/b/l/a:dh') do (
+if %%~nf==__history rd /s/q %%f
+)
+
