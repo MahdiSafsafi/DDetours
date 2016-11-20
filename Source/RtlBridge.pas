129c62669d4cff796d838a1dcecbaeda4c6da49c
MahdiSafsafi <ismspider@gmail.com>
2016-11-20 13:31:58 +0100

    Added cross platform lock functionality.

--- Source/RtlBridge.pas
+++ Source/RtlBridge.pas
@@ -26,7 +26,18 @@ uses
     ;
 
 type
+  TLock = record
+{$IFDEF UseCriticalsection}
+    Criticalsection: TRTLCriticalSection;
+{$ELSE !UseCriticalsection}
+    case Boolean of
+      False: (LockValue: NativeUInt);
+      True: (LockObj: TObject);
+{$ENDIF UseCriticalsection}
+  end;
+
 {$IFDEF FPC}
+
   TRegExpression = TRegExpr;
 {$ELSE !FPC}
 
@@ -44,8 +55,110 @@ type
 {$ENDIF FPC}
 
 procedure FindAllDirectories(const Path: string; AList: TStringList; WalkSubDirectories: Boolean);
+procedure InitLock(var LockStruct: TLock);
+procedure FinalizeLock(var LockStruct: TLock);
+procedure Lock(var LockStruct: TLock);
+procedure Unlock(var LockStruct: TLock);
 
 implementation
+
+{$IFDEF FPC}
+// {$MODE Delphi}
+{$ASMMODE intel}
+{$ENDIF FPC}
+{$IFDEF UseSimpleLock}
+
+function BoolCmpExchange(Address: Pointer; CompareValue, NewValue: Boolean): Boolean; assembler;
+asm
+  {$IFDEF CPUX32}
+  { eax = Address
+  cl    =  CompareValue
+  dl    =  NewValue }
+  xchg eax,ecx
+  lock cmpxchg byte [ecx],dl
+  {$ELSE !CPUX32}
+  { rcx = Address
+  dl    = CompareValue
+  r8b   = NewValue }
+  mov al,dl
+  lock cmpxchg byte [rcx],r8b
+  {$ENDIF CPUX32}
+end;
+{$ENDIF UseSimpleLock}
+
+procedure InitLock(var LockStruct: TLock);
+begin
+{$IF DEFINED (UseMonitor)}
+  LockStruct.LockObj := TObject.Create;
+{$ELSEIF DEFINED (UseCriticalsection)}
+{$IF DEFINED (FPC)}
+  InitCriticalSection(LockStruct.Criticalsection);
+{$ELSEIF DEFINED(MSWINDOWS)}
+  InitializeCriticalSection(LockStruct.Criticalsection);
+{$ENDIF FPC}
+{$ELSEIF DEFINED (UseSimpleLock)}
+  LockStruct.LockValue := 0;
+{$ENDIF}
+end;
+
+procedure FinalizeLock(var LockStruct: TLock);
+begin
+{$IF DEFINED (UseMonitor)}
+  FreeAndNil(LockStruct.LockObj);
+{$ELSEIF DEFINED (UseCriticalsection)}
+{$IF DEFINED (FPC)}
+  DoneCriticalSection(LockStruct.Criticalsection);
+{$ELSEIF DEFINED (MSWINDOWS)}
+  DeleteCriticalSection(LockStruct.Criticalsection);
+{$ENDIF FPC}
+{$ELSEIF DEFINED (UseSimpleLock)}
+  LockStruct.LockValue := 0;
+{$ENDIF}
+end;
+
+procedure Lock(var LockStruct: TLock);
+{$IFDEF UseSimpleLock}
+const
+  SleepTime = 20;
+{$ENDIF UseSimpleLock}
+begin
+{$IF DEFINED (UseMonitor)}
+  System.TMonitor.Enter(LockStruct.LockObj);
+{$ELSEIF DEFINED (UseCriticalsection)}
+{$IF DEFINED (FPC)}
+  System.EnterCriticalsection(LockStruct.Criticalsection);
+{$ELSEIF DEFINED(MSWINDOWS)}
+  Windows.EnterCriticalsection(LockStruct.Criticalsection);
+{$ELSE}
+  Error();
+{$ENDIF FPC}
+{$ELSEIF DEFINED (UseSimpleLock)}
+  if IsMultiThread then
+  begin
+    while BoolCmpExchange(@LockStruct.LockValue, False, True) do
+    begin
+      Sleep(SleepTime);
+    end;
+  end;
+{$ELSE}
+  Error();
+{$ENDIF}
+end;
+
+procedure Unlock(var LockStruct: TLock);
+begin
+{$IF DEFINED (UseMonitor)}
+  System.TMonitor.Exit(LockStruct.LockObj);
+{$ELSEIF DEFINED (UseCriticalsection)}
+{$IF DEFINED (FPC)}
+  System.LeaveCriticalSection(LockStruct.Criticalsection);
+{$ELSEIF DEFINED(MSWINDOWS)}
+  Windows.LeaveCriticalSection(LockStruct.Criticalsection);
+{$ENDIF FPC}
+{$ELSEIF DEFINED (UseSimpleLock)}
+  LockStruct.LockValue := 0;
+{$ENDIF}
+end;
 
 procedure FindAllDirectories(const Path: string; AList: TStringList; WalkSubDirectories: Boolean);
 {$IFNDEF FPC}
